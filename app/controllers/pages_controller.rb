655689d48bd64e7428babc5e6c8536857ec8c9a5
yablko <yablko@brm.sk>
2022-03-29 20:30:53 +0200

    04-RAILS-MVC

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @message = "tu sme naozaj doma!!"
   end
 
   def contact
+    @message = "nepíš mi, fakt 😡"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'pages/home'
-  get 'pages/contact'
+  root "pages#home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/contact', to: 'pages#contact'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
