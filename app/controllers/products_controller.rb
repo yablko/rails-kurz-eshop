dcdf790a2bb7eac81fa59dddf4f8c7cf088dc52e
yablko <yablko@brm.sk>
2022-03-30 15:45:35 +0200

    16-IMAGE-UPLOAD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :description, :price)
+      params.require(:product).permit(:title, :description, :price, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
   has_rich_text :description
 end
