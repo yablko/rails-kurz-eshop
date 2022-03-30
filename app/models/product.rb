0ae4daf0d3a51f2eb7f936af24febcb9349035f6
yablko <yablko@brm.sk>
2022-03-30 16:38:06 +0200

    17-THUMBNAIL-FILE-VARIANTS

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb,
+      crop: [300, 700, 1280, 900],
+      resize_to_fit: [600, 600]
+  end
 
   has_rich_text :description
 end
