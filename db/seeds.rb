db74bb73d41889ac583aadd99ecd363de03393d9
yablko <yablko@brm.sk>
2022-04-01 20:28:11 +0200

    27-DB-SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.all.each do |product|
+  puts product.id
+
+  rand(5).times do |i|
+    content = "beatiful review number #{i+1}"
+    rating = rand(0..5)
+
+    product.reviews.create! content: content, rating: rating
+    puts "review created"
+  end
+
+  puts "--"
+end
