cd5ee5c05007181add3b870ac40129a7ae2ec1cb
yablko <yablko@brm.sk>
2022-04-01 17:51:17 +0200

    21-FORM-VALIDATION

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: true
+
   has_one_attached :image do |attachable|
     attachable.variant :thumb,
       crop: [300, 700, 1280, 900],
